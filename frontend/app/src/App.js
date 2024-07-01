7b9250c3a055f8b3b5a76981c5e1b9d33a15eac1
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-07-01 20:32:49 +0300

    update page content alignment

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Box } from '@mui/material';
+
 import Searchbar from './components/search';
 
 function App() {
   return (
-    <div className="App">
-      <Searchbar />       
-    </div>
+   <Box 
+   component="section" 
+   sx={{ p: 4, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <Searchbar />
+   </Box>
   );
 }
 
