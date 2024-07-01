4bf592ff62db4f75d3efd54b78ff915e01717b19
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-07-01 22:13:48 +0300

    setup gql

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { Box } from '@mui/material';
-
+import { useQuery, gql } from '@apollo/client';
+import { Box, Typography } from '@mui/material';
 import Searchbar from './components/Search';
 import BookGrid from './components/BookItem';
 
-function App() {
+const GET_BOOKS = gql`
+  query Books {
+    books {
+      author
+      coverPhotoURL
+      readingLevel
+      title
+    }
+  }
+`;
+
+const App = () => {
+  const {loading, error, data} = useQuery(GET_BOOKS);
+
+  if(loading) return <Typography>Loading...</Typography>
+
+  if(error) return <Typography>Error!</Typography>
+
   return (
    <Box 
    component="section" 
