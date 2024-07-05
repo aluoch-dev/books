bdc8c17cddcda12f014463be55dc9352a5d97a66
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-07-05 09:43:41 +0300

    update button to use theme colors

--- frontend/app/src/components/Buttons.js
+++ frontend/app/src/components/Buttons.js
@@ -5,7 +5,17 @@ import { Button } from "@mui/material";
 const PrimaryButton = ({ text }) => {
 
     return (
-        <Button variant="contained" color="primary">
+        <Button 
+        variant="contained" 
+        color="primary" 
+        sx={{
+            fontFamily: 'Mulish, sans-serif',
+            borderRadius: '80px',
+            backgroundColor: (theme) => theme.palette.turquiose.primary,
+            '&:hover': {
+              backgroundColor: (theme) => theme.palette.turquiose.tertiary,
+            },
+          }}>
         {text}
       </Button>
     )
