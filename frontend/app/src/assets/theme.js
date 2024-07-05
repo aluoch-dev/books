4c2dfaea0b8189b998e495efc828841ed6bae15e
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-07-05 09:21:16 +0300

    setup project theme

---
+++ frontend/app/src/assets/theme.js
@@ -0,0 +1,26 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    typography: {
+        
+    },
+    palette: {
+        turquiose: {
+            light:'#CFFAFA',
+            primary:'#5ACCCC',
+            secondary:'#CFFAFA',
+            tertiary: '#28B8B8'
+        },
+        orange: {
+            light: '#FFE6DC',
+            tertiary: '#F76434'
+        },
+        yellow: {
+            primary:'#FABD33',
+            secondary: '#FAAD00'
+        }
+    }
+
+});
+
+export default theme;
